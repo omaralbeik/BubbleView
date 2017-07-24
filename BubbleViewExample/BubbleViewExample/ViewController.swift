1d438cc3613f4bc5fbfb6623e3416ec606a0fd8d
Omar Albeik <omaralbeik@gmail.com>
2017-07-24 21:45:57 +0300

    Updated tail shape

---
+++ BubbleViewExample/BubbleViewExample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  BubbleViewExample
+//
+//  Created by Omar Albeik on 7/24/17.
+//  Copyright © 2017 Omar Albeik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var bubbleView: BubbleView!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
